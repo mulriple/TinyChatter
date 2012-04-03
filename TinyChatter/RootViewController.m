50b184591028f87e70ea8812559d203f4bf0b122
Jason Ling <jtg2078@gmail.com>
2012-04-03 16:03:03 +0800

    some more libraries and a RootViewController

---
+++ TinyChatter/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  TinyChatter
+//
+//  Created by jj on 12/4/3.
+//  Copyright (c) 2012年 jtg2078@hotmail.com. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
